46c2865c5db4f4e6c3f75ad40e8803c54fcdace4
Paul Crooks <pgcrooks@gmail.com>
2020-12-04 16:44:27 +0000

    Extend find_sum to work with many window sizes
    This needs to be moved to a template.

--- advent/src/lib.rs
+++ advent/src/lib.rs
@@ -32,6 +32,31 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
+fn find_sum_2(numbers: &[String], desired: i32) -> Option<i32> {
+    for win in numbers.windows(2) {
+        let a: i32 = win[0].parse().unwrap();
+        let b: i32 = win[1].parse().unwrap();
+        if a + b == desired {
+            println!("find_sum_2 {} + {} = {}", a, b, desired);
+            return Some(a * b)
+        }
+    }
+    None
+}
+
+fn find_sum_3(numbers: &[String], desired: i32) -> Option<i32> {
+    for win in numbers.windows(3) {
+        let a: i32 = win[0].parse().unwrap();
+        let b: i32 = win[1].parse().unwrap();
+        let c: i32 = win[2].parse().unwrap();
+        if a + b + c == desired {
+            println!("find_sum_3 {} + {} + {} = {}", a, b, c, desired);
+            return Some(a * b * c)
+        }
+    }
+    None
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut lines = lines_from_file(config.filename)?;
 
@@ -40,17 +65,22 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut rng = thread_rng();
     let mut done = false;
     while !done {
-        &lines.shuffle(&mut rng);
-
-        for win in lines.windows(2) {
-            let a: i32 = win[0].parse().unwrap();
-            let b: i32 = win[1].parse().unwrap();
-            if a + b == desired {
-                println!("Found it! {} + {} = {}", a, b, desired);
-                println!("Answer = {}", a * b);
-                done = true;
-            }
+        let result = find_sum_2(&lines, desired);
+        if let Some(i) = result {
+            println!("Found 2 numbers! Answer = {}", i);
+            done = true;
         }
+        &lines.shuffle(&mut rng);
+    }
+    
+    let mut done = false;
+    while !done {
+        let result = find_sum_3(&lines, desired);
+        if let Some(i) = result {
+            println!("Found 3 numbers! Answer = {}", i);
+            done = true;
+        }
+        &lines.shuffle(&mut rng);
     }
 
     Ok(())
