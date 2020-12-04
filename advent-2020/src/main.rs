94564c08dc49ee024df6fd6946aad746f991af29
Paul Crooks <pgcrooks@gmail.com>
2020-12-04 14:26:24 +0000

    Add error handling instead of panic to args

--- advent-2020/src/main.rs
+++ advent-2020/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Advent 2020");
     println!("Input file: {:?}", config.filename);
@@ -20,14 +24,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 2 {
-            panic!("Filename argument required");
+            return Err("Filename argument required");
         }
         if args.len() > 2 {
-            panic!("Too many arguments");
+            return Err("Too many arguments");
         }
+
         let filename = args[1].clone();
-        Config { filename }
+
+        Ok(Config { filename })
     }
 }
