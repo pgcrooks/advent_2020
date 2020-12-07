c433600a6c9cf77e07dcb2ce29edd0ef0bbc36b3
Paul Crooks <pgcrooks@gmail.com>
2020-12-07 14:17:07 +0000

    Change day 2 password algorithm

--- advent/src/day_2.rs
+++ advent/src/day_2.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
+use std::convert::TryInto;
 
 use advent::Config;
 
-struct NumberAllowed {
-    pub min: usize,
-    pub max: usize,
+struct Positions {
+    pub first: usize,
+    pub second: usize,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -19,17 +20,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
         let number_str = String::from(split_vec[0]);
         let number_vec = number_str.split("-").collect::<Vec<&str>>();
-        let allowed_count = NumberAllowed {
-            min: number_vec[0].parse().unwrap(),
-            max: number_vec[1].parse().unwrap()
+        let positions = Positions {
+            first: number_vec[0].parse().unwrap(),
+            second: number_vec[1].parse().unwrap()
         };
 
         let character = String::from(split_vec[1]).remove(0);
         let data = String::from(split_vec[2]);
 
-        let char_count = data.matches(character).count();
-        if char_count >= allowed_count.min && char_count <= allowed_count.max {
-            valid_password_count += 1;
+        // Ignore passwords that are too short
+        // Data is 1-indexed
+        if (data.len() + 1) >= positions.second.try_into().unwrap() {
+            let string_as_bytes = data.as_bytes();
+            let first_unicode: u8 = string_as_bytes[positions.first - 1];
+            let first_char: char = first_unicode as char;
+            let second_unicode: u8 = string_as_bytes[positions.second - 1];
+            let second_char: char = second_unicode as char;
+
+            if (first_char == character) ^ (second_char == character) {
+                valid_password_count += 1;
+            }
         }
     }
 
